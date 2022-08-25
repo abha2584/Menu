00db556afa1089b79bcfc400df60c86d15fda9fb
Abha <abha2584@yahoo.com>
2022-08-24 20:51:34 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './interface/menu-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'menu';
+  title = 'Welcome to Chirpus Pizza!';
+menuitems:MenuItem[] = [
+  {Item:'Chicken Fingers',Category:'dinner',Price:11.99},
+  {Item:'Pizza',Category:'dinner',Price:11.99},
+  {Item:'Wings',Category:'sides',Price:8.99},
+  {Item:'Breadsticks',Category:'sides',Price:4.99},
+  {Item:'Ceaser Salad',Category:'salads',Price:5.99},
+  {Item:'Cinnamon Roll',Category:'dessert',Price:8.99}
+];
+
+
 }
